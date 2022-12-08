898e98ab38b6dd58f240c2bddcc309b58b9528c4
sudoMan <kyawphyothukpt256@gmail.com>
2022-12-08 16:14:09 +0630

    pagination update v2.0

--- resources/views/vendor/pagination/custom.blade.php
+++ resources/views/vendor/pagination/custom.blade.php
@@ -4,7 +4,7 @@
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
                 <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                    <span class="page-link" aria-hidden="true">Last</span>
+                    <span class="page-link" aria-hidden="true">First</span>
                 </li>
             @else
                 <li class="page-item">
